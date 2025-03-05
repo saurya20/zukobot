f6ef217be2d20f6b5468a10af8c4789ef5c3f3ae
saurya <sauryaps20@gmail.com>
2025-03-05 10:40:11 +0530

    first commit

---
+++ BotManager.py
@@ -0,0 +1,16 @@
+import configs.DefaultConfig as defaultConfig
+import utils.DiscordUtil as discordUtil
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+intents.message_content = True
+intents.members = True
+bot = commands.Bot(command_prefix="!",intents=intents,help_command=None)
+
+@bot.event
+async def on_ready():
+    print("Bot is online..")
+
+bot.run(defaultConfig.DISCORD_SDK)
